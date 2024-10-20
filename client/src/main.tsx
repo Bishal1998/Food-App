fdb3dfa8eb1ca8626db6380da5269201b6c21849
bishal1998 <bishalpaudel1998@gmail.com>
2024-10-20 11:44:45 -0400

    Navbar Created

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 import Login from "@/auth/Login.tsx";
@@ -8,11 +7,12 @@ import Signup from "@/auth/Signup.tsx";
 import ForgotPassword from "@/auth/ForgotPassword.tsx";
 import ResetPassword from "@/auth/ResetPassword.tsx";
 import VerifyEmail from "@/auth/VerifyEmail.tsx";
+import Navbar from "@/components/Navbar.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App/>
+        element: <Navbar/>
     },
     {
         path: "/login",
