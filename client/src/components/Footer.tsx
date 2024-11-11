ccb4c4c8e8d21111fe1d02040f7e8a7d97bcc7e4
bishal1998 <bishalpaudel1998@gmail.com>
2024-11-10 21:18:35 -0500

    Footer Component completed

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-center text-gray-300">
+      <p className="text-sm">&copy; 2024 FoodApp. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
