245744ee93dc383ca66b5b9a3a686ed116af1625
bishal1998 <bishalpaudel1998@gmail.com>
2024-11-10 21:19:36 -0500

    main file updated with the layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,43 +1,50 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 import Login from "@/auth/Login.tsx";
 import Signup from "@/auth/Signup.tsx";
 import ForgotPassword from "@/auth/ForgotPassword.tsx";
 import ResetPassword from "@/auth/ResetPassword.tsx";
 import VerifyEmail from "@/auth/VerifyEmail.tsx";
-import Navbar from "@/components/Navbar.tsx";
+import MainLayout from "@/layout/MainLayout";
+import HeroSection from "@/components/HeroSection";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
         path: "/",
-        element: <Navbar/>
-    },
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/signup",
-        element: <Signup/>
-    },
-    {
-        path: "/forgot-password",
-        element: <ForgotPassword/>
-    },
-    {
-        path: "/reset-password",
-        element: <ResetPassword/>
-    },
-    {
-        path: "/verify-email",
-        element: <VerifyEmail/>
-    },
-])
+        element: <HeroSection />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/reset-password",
+    element: <ResetPassword />,
+  },
+  {
+    path: "/verify-email",
+    element: <VerifyEmail />,
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <RouterProvider router={router}/>
-    </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
