cc272e765046c303166e1a91e87b7a826777abcc
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-12-25 15:26:13 -0500

    FilterPage Component Completed

--- client/src/components/FilterPage.tsx
+++ client/src/components/FilterPage.tsx
@@ -1,5 +1,50 @@
+import { Label } from "@radix-ui/react-menubar";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+
+export interface IFilterOptions {
+  id: string;
+  label: string;
+}
+
+const filterOptions: IFilterOptions[] = [
+  {
+    id: "thali",
+    label: "Thali",
+  },
+  {
+    id: "burger",
+    label: "Burger",
+  },
+  {
+    id: "pizza",
+    label: "Pizza",
+  },
+  {
+    id: "biryani",
+    label: "Biryani",
+  },
+];
+
 const FilterPage = () => {
-  return <div>FilterPage</div>;
+  const filterHandler = (value: string) => {};
+  return (
+    <div className="md:w-72">
+      <div className="flex items-center justify-between">
+        <h1 className="font-medium text-lg">Filter by Cuisines</h1>
+        <Button variant={"link"}>Reset</Button>
+      </div>
+      {filterOptions.map((option) => (
+        <div key={option.id} className="flex items-center space-x-2 my-5">
+          <Checkbox
+            id={option.id}
+            onClick={() => filterHandler(option.label)}
+          />
+          <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">{option.label}</Label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FilterPage;
