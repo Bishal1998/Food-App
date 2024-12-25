13e7cc944fca0f5500809c73cb3268b13f8bff33
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-12-25 16:08:25 -0500

    ResturantMenu UI completed

--- client/src/components/RestaurantMenu.tsx
+++ client/src/components/RestaurantMenu.tsx
@@ -1,5 +1,36 @@
+import { Button } from "./ui/button";
+import { Card, CardContent, CardFooter } from "./ui/card";
+import Pizza from "@/assets/pizza.jpg";
+
 const RestaurantMenu = () => {
-  return <div>RestaurantMenu</div>;
+  return (
+    <div className="md:p-4">
+      <h1 className="text-xl md:text-2xl font-extrabold mb-6">
+        Available Menus
+      </h1>
+      <div className="grid md:grid-cols-3 space-y-4 md:space-y-0">
+        <Card className="max-w-xs mx-auto shadow-lg rounded-lg overflow-hidden">
+          <img src={Pizza} alt="" className="w-full h-40 object-cover" />
+          <CardContent className="p-4">
+            <h2 className="text-xl font-semibold text-gray-800 dark:text-white">
+              Biryani
+            </h2>
+            <p className="text-sm text-gray-600 mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <h3 className="text-lg font-semibold mt-4">
+              Price: <span className="text-[#D19254]">$80</span>
+            </h3>
+          </CardContent>
+          <CardFooter className="p-4">
+            <Button className="bg-orange hover:bg-hoverOrange w-full">
+              Add to Cart
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantMenu;
