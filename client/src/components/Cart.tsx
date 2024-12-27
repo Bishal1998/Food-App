6c242ef4b148350ca0abb9d4e91ece591258c4ef
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-12-26 23:07:26 -0500

    Cart component completed

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,8 +1,91 @@
+import { Minus, PlusIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="flex flex-col max-w-7xl mx-auto my-10">
+      <div className="flex justify-end">
+        <Button variant={"link"}>Clear All</Button>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Item</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead className="text-right">Remove</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <Avatar>
+                <AvatarImage src="" alt=""></AvatarImage>
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </TableCell>
+            <TableCell>Tanduri Biryani</TableCell>
+            <TableCell>$80</TableCell>
+            <TableCell>
+              <div className="w-fit flex items-center rounded-full border-gray-100 dark:border-gray-800 shadow-md">
+                <Button
+                  size={"icon"}
+                  className="rounded-full bg-gray-200"
+                  variant={"outline"}
+                >
+                  <Minus></Minus>
+                </Button>
+                <Button
+                  disabled
+                  variant={"outline"}
+                  size={"icon"}
+                  className="font-bold border-none"
+                >
+                  1
+                </Button>
+                <Button
+                  size={"icon"}
+                  variant={"outline"}
+                  className="rounded-full bg-orange hover:bg-hoverOrange"
+                >
+                  <PlusIcon></PlusIcon>
+                </Button>
+              </div>
+            </TableCell>
+            <TableCell>80</TableCell>
+            <TableCell className="text-right">
+              <Button size={"sm"} className="bg-orange hover:bg-hoverOrange">
+                Remove
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={5}>Total</TableCell>
+            <TableCell className="text-right">$80</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+      <div className="flex justify-end my-5">
+        <Button className="bg-orange hover:bg-hoverOrange">
+          Proceed to Checkout
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
