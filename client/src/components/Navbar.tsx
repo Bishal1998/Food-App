b9a213cb815a23346b3ea129b5222211f75fcef2
Bishal Paudel <bishalpaudel1998@gmail.com>
2024-12-27 00:25:57 -0500

    Restaurant admin link updated

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -190,7 +190,7 @@ const MobileNav = () => {
             <span>Menu</span>
           </Link>
           <Link
-            to="/profile"
+            to="/admin/restaurant"
             className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 cursor-pointer hover:text-gray-900 font-medium"
           >
             <UtensilsCrossed />
