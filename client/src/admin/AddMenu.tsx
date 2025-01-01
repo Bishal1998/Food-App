96aa38d7aecd3c396a881339f926e975b1476c1d
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-01-01 15:34:39 -0500

    AddMenu form functionality checked

--- client/src/admin/AddMenu.tsx
+++ client/src/admin/AddMenu.tsx
@@ -11,14 +11,45 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2, Plus } from "lucide-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Pizza from "@/assets/pizza.jpg";
 import EditMenu from "@/admin/EditMenu";
 
+const menus = [
+  {
+    title: "Pizza",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quasi at nobis iste magni officia cum corporis assumenda rem repellat.",
+    price: "80",
+    img: Pizza,
+  },
+];
+
 const AddMenu = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [selectedMenu, setSelectedMenu] = useState();
+
+  const [inputData, setInputData] = useState<any>({
+    name: "",
+    desc: "",
+    price: 0,
+    img: "",
+  });
 
   const loading = false;
+
+  const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type } = e.target;
+    setInputData({
+      ...inputData,
+      [name]: type === "number" ? Number(value) : value,
+    });
+  };
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(inputData);
+  };
+
   return (
     <div className="max-w-6xl mx-auto my-10">
       <div className="flex justify-between">
@@ -39,26 +70,49 @@ const AddMenu = () => {
                 Create a menu that will make your restaurant standout.
               </DialogDescription>
             </DialogHeader>
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={submitHandler}>
               <div>
                 <Label>Name</Label>
-                <Input type="text" name="name" placeholder="Enter menu name" />
+                <Input
+                  type="text"
+                  name="name"
+                  value={inputData.name}
+                  placeholder="Enter menu name"
+                  onChange={changeEventHandler}
+                />
               </div>
               <div>
                 <Label>Description</Label>
                 <Input
                   type="text"
                   name="desc"
+                  value={inputData.desc}
+                  onChange={changeEventHandler}
                   placeholder="Enter menu description"
                 />
               </div>
               <div>
                 <Label>Price in Rs.</Label>
-                <Input type="text" name="price" placeholder="Enter price" />
+                <Input
+                  type="number"
+                  name="price"
+                  placeholder="Enter price"
+                  value={inputData.price}
+                  onChange={changeEventHandler}
+                />
               </div>
               <div>
                 <Label>Upload Menu Image</Label>
-                <Input type="file" name="img" placeholder="Enter menu Image" />
+                <Input
+                  type="file"
+                  name="img"
+                  onChange={(e) =>
+                    setInputData({
+                      ...inputData,
+                      img: e.target.files?.[0] || undefined,
+                    })
+                  }
+                />
               </div>
               <DialogFooter className="mt-5">
                 {loading ? (
@@ -76,28 +130,37 @@ const AddMenu = () => {
           </DialogContent>
         </Dialog>
       </div>
-      <div className="mt-6 space-y-4">
-        <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
-          <img
-            src={Pizza}
-            alt=""
-            className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
-          />
-          <div className="flex-1">
-            <h1 className="text-lg font-semibold text-gray-800">Pizza</h1>
-            <p className="text-sm text-gray-600 mt-1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-            <h2 className="text-md font-semibold mt-2">
-              Price: <span className="text-[#D19254]">$80</span>
-            </h2>
+      {menus.map((menu: any, index: number) => {
+        return (
+          <div key={index} className="mt-6 space-y-4">
+            <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
+              <img
+                src={menu.img}
+                alt=""
+                className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
+              />
+              <div className="flex-1">
+                <h1 className="text-lg font-semibold text-gray-800">
+                  {menu.title}
+                </h1>
+                <p className="text-sm text-gray-600 mt-1">{menu.desc}</p>
+                <h2 className="text-md font-semibold mt-2">
+                  Price: <span className="text-[#D19254]">${menu.price}</span>
+                </h2>
+              </div>
+              <Button
+                onClick={() => setSelectedMenu()}
+                size={"sm"}
+                className="bg-orange hover:bg-hoverOrange mt-2"
+              >
+                Edit
+              </Button>
+            </div>
           </div>
-          <Button size={"sm"} className="bg-orange hover:bg-hoverOrange mt-2">
-            Edit
-          </Button>
-        </div>
-      </div>
-      <EditMenu />
+        );
+      })}
+
+      <EditMenu seletedMenu={selectedMenu} />
     </div>
   );
 };
