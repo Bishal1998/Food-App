002dfbba62263809e2c1592c80ba99a860aa49ee
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-01-09 22:41:57 -0500

    UpdateProfile created

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import User from "../models/user.model";
 import bcryptjs from "bcryptjs";
 import crypto from "crypto";
+import cloudinary from "../utils/cloudinary";
 
 const signup = async (req: Request, res: Response) => {
   try {
@@ -242,7 +243,30 @@ const updateProfile = async (req: Request, res: Response) => {
 
     // upload image on cloudinary
 
-    
+    let cloudResponse: any;
+
+    try {
+      cloudResponse = await cloudinary.uploader.upload(profilePicture);
+
+      const updatedData = {
+        fullName,
+        email,
+        address,
+        city,
+        country,
+        profilePicture,
+      };
+
+      const user = await User.findById(userId, updatedData, {
+        new: true,
+      }).select("-password");
+
+      return res
+        .status(200)
+        .json({ success: true, message: "Profile updated successfully", user });
+    } catch (error) {
+      console.log("Cloudinary error: ", error);
+    }
   } catch (error) {
     console.log("Update Profile error: ", error);
     return res
@@ -251,4 +275,12 @@ const updateProfile = async (req: Request, res: Response) => {
   }
 };
 
-export { signup, login, verifyEmail, logout, forgotPassword, resetPassword };
+export {
+  signup,
+  login,
+  verifyEmail,
+  logout,
+  forgotPassword,
+  resetPassword,
+  updateProfile,
+};
