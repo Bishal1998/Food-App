af3e5db82d2ba75246f60b22e7b2e3ca4cbf4d9e
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-02-06 21:32:57 -0500

    AddMenu controller created

--- server/controller/menu.controller.ts
+++ server/controller/menu.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import uploadImageOnCloudinary from "../utils/imageUpload";
 import Menu from "../models/menu.model";
+import Restaurant from "../models/restaurant.model";
+import mongoose, { Mongoose } from "mongoose";
 
 const addMenu = async (req: Request, res: Response) => {
   try {
@@ -13,7 +15,22 @@ const addMenu = async (req: Request, res: Response) => {
     }
 
     const imageUrl = await uploadImageOnCloudinary(file as Express.Multer.File);
-    const menu = await Menu
+    const menu = await Menu.create({
+      name,
+      description,
+      price,
+      image: imageUrl,
+    });
+
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (restaurant) {
+      (restaurant.menus as mongoose.Schema.Types.ObjectId[]).push(menu._id);
+      await restaurant.save();
+    }
+
+    res
+      .status(201)
+      .json({ success: true, message: "Menu added successfully.", menu });
   } catch (error) {
     console.log("Add Menu Error : ", error);
     res.status(500).json({ message: "Internal Server Error" });
